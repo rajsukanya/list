860212119ae1d25166681f0b123193637a3edf39
rajsukanya <sukanyaraj1014@yahoo.com>
2018-08-13 18:03:59 -0400

    Has errors

--- memo_batch_tester.cpp
+++ memo_batch_tester.cpp
@@ -11,88 +11,83 @@ using namespace std;
 
 int memo_batch_tester(int argc, char *argv[])
 {
-  cout << "Inside memo batch tester" <<endl;
-  /*
-  for(int i = 0; i < argc-1; i++)
-  {
-    cout << argv[i] <<endl;
-  }
-  */
-  /*
   ifstream inStream;
-  string line, tag;
+  string line, tag, file;
   list_item_type new_item;
-  bool expected, success, actual;
+  bool success, actual;
   int position, new_position, DataItem;
-  int i, value, lineNum, numError = 0;
+  int i, value, numError = 0;
 
-for(i = 0; i < argc; i++)
-{  
-  inStream.open(argv[0]);
-  if(inStream.fail())
+  for(i = 0; i < argc; i++)
   {
-    cout << "Failed to open file" <<endl;
-    exit(1);
+    file = argv[1];
+    inStream.open(file.c_str());
+    if(inStream.fail())
+    {
+      cout << "Failed to open file" <<endl;
+      exit(1);
+    }
   }
-
-  lineNum = 0;
   while(getline(inStream, line))
   {
-    //lineNum++;
     inStream >> tag >> value;
-    cout << "TAG: " << tag <<endl;
-    cout << "VALUE: " << value <<endl;
+    //cout << "TAG: " << tag <<endl;
+    //cout << "VALUE: " << value <<endl;
     
     if(tag == "NUM_OPERATIONS")
     {
-      int op[value], key[value], expected_status[value];
+      int op[value], key[value], t;
+      bool expected_status[value];
 
       for(i = 0; i < value; i++)
       {
-        inStream >> op[i] >> key[i] >> expected_status[i];
-      
-        if(expected_status[i] == 0)
-          expected = false;
-        if(expected_status[i] == 1)
-          expected = true;
+        inStream >> op[i] >> key[i] >> t;
+
+        if(t == 1)
+        {
+          expected_status[i] = true;
+        }
+        else
+        {
+          expected_status[i] = false;
+        }
+        //cout << expected_status[i] <<endl;
       }
       inStream.close();
-      
-      i = 0;
-      while(i < value)
-      {  
+    
+      for(i = 0; i < value; i++)
+      {
         switch(op[i])
         {
-          case READ:
+          case READ: 
             retrieve(position, DataItem, success);
-          case WRITE:
+            break;
+          case WRITE: 
             insert(new_position, new_item, success);
-          case DELETE:
+            break;
+          case DELETE: 
             remove(position, success);
-          default:
+            break;
+          default: 
             cout << "Error" <<endl;
             break;
-         }
-       
-         actual = get_status();
-         if(actual == false && expected == false)
-           cout << "Good" <<endl;
-         if(actual == true && expected == true)
-            cout << "Good" <<endl;
-         if(actual == false && expected == true)
-         {
-            cout << "Error" <<endl;
-            numError++;
-         }
-         if(actual == true && expected == false)
-         {
-           cout << "Error" <<endl;
-           numError++;
-         }
-         i++;
+        }
+        actual = get_status();
+        if(actual == false && expected_status[i] == false)
+          cout << "Good" <<endl;
+        if(actual == true && expected_status[i] == true)
+          cout << "Good" <<endl;
+        if(actual == false && expected_status[i] == true)
+        {
+          cout << "Error" <<endl;
+          numError++;
+        }
+        if(actual == true && expected_status[i] == false)
+        {
+          cout << "Error" <<endl;
+          numError++;
+        }
       }
-     }
     }
-}
-*/
+  }
 }
